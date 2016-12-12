74a5f21fa9c05c06d734360f957c1b11d6ac0398
chrisbacon <bacon.chris@gmail.com>
2016-12-12 15:10:37 +0000

    Created tests for wrestler class

--- app/src/test/java/com/codeclan/example/wrestlingrankings/WrestlerTest.java
+++ app/src/test/java/com/codeclan/example/wrestlingrankings/WrestlerTest.java
@@ -1,9 +1,48 @@
 package com.codeclan.example.wrestlingrankings;
 
+import org.junit.*;
+import static org.junit.Assert.*;
 /**
  * Created by user on 12/12/2016.
  */
 public class WrestlerTest {
     Wrestler wrestler;
-    @Before 
+
+    @Before
+    public void before() {
+        this.wrestler = new Wrestler("Stone Cold Steve Austin", "Stone Cold Stunner", 1);
+    }
+
+    @Test
+    public void getNameTest() {
+        assertEquals("Stone Cold Steve Austin", wrestler.getName());
+    }
+
+    @Test
+    public void setNameTest() {
+        wrestler.setName("Stunning Steve Austin");
+        assertEquals("Stunning Steve Austin", wrestler.getName());
+    }
+
+    @Test
+    public void getFinisherTest() {
+        assertEquals("Stone Cold Stunner", wrestler.getFinisher());
+    }
+
+    @Test
+    public void setFinisherTest() {
+        wrestler.setFinisher("Stun Gun");
+        assertEquals("Stun Gun", wrestler.getFinisher());
+    }
+
+    @Test
+    public void getRankingTest() {
+        assertEquals(1, wrestler.getRanking());
+    }
+
+    public void setRankingTest() {
+        wrestling.setRanking(2);
+        assertEquals(2, wrestler.getRanking());
+    }
+
 }
